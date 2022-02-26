eaab5c8bb4f358b15bee554d1700734a19099813
corskaya <cagriorskaya@hotmail.com>
2022-02-26 22:40:37 +0300

    dynamic data implemented

--- Notes-App/notes-app.js
+++ Notes-App/notes-app.js
@@ -1,3 +1,14 @@
+const notes = [{
+  title: 'My next trip',
+  body: 'I would like to go to Spain'
+}, {
+  title: 'Habbits to work on',
+  body: 'Exercise, Eating a bit better.'
+}, {
+  title: 'Office modification',
+  body: 'Get a new seat'
+}]
+
 // DOM - Document Object Model
 
 // Query and remove
@@ -10,3 +21,8 @@ ps.forEach(function (p) {
   p.textContent = '*****'
   // p.remove()
 })
+
+// Add a new element
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This is a new element from JavaScript'
+document.querySelector('body').appendChild(newParagraph)
--- Todo-App/script.js
+++ Todo-App/script.js
@@ -1,12 +1,33 @@
-// 1) Create a new HTML file
-// 2) An h1 and 5 p tags
-// 3) Server that folder and view the doc in the browser
+const todos = [{
+  text: 'Order cat food',
+  completed: true
+}, {
+  text: 'Clean kitchen',
+  completed: false
+}, {
+  text: 'Buy food',
+  completed: true
+}, {
+  text: 'Do work',
+  completed: false
+}, {
+  text: 'Exercise',
+  completed: true
+}]
 
-const h1 = document.querySelector('h1')
-const ps = document.querySelectorAll('p')
+// Challenge
+// You have 2 todos to left (p element)
+// Add a p for each todo above (use text value)
 
-ps.forEach(p => {
-  if (p.textContent.includes('the')) {
-    p.remove()
-  }
+const leftToDo = todos.filter(todo => !todo.completed)
+
+let explanation = document.createElement('h3')
+explanation.textContent = `You have ${leftToDo.length} todos left`
+document.querySelector('body').appendChild(explanation)
+
+let thing
+leftToDo.forEach(todo => {
+  thing = document.createElement('p')
+  thing.textContent = `- ${todo.text}`
+  document.querySelector('body').appendChild(thing)
 })
