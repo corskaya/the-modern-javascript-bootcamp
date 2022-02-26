50c0b174f5d31af803988232d40da66822a2a45f
corskaya <cagriorskaya@hotmail.com>
2022-02-26 21:53:07 +0300

    dom manipulation implemented

--- Notes-App/notes-app.js
+++ Notes-App/notes-app.js
@@ -1,1 +1,12 @@
-console.log('This is from a different file')
+// DOM - Document Object Model
+
+// Query and remove
+// const p = document.querySelector('p')
+// p.remove()
+
+// Query all and remove
+const ps = document.querySelectorAll('p')
+ps.forEach(function (p) {
+  p.textContent = '*****'
+  // p.remove()
+})
