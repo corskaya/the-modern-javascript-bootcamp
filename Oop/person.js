5bcdc19b91049bbf9410d6963a70a28184028060
corskaya <cagriorskaya@hotmail.com>
2022-03-03 17:58:21 +0300

    subclasses created

--- Oop/person.js
+++ Oop/person.js
@@ -25,10 +25,55 @@ class Person {
   }
 }
 
-const me = new Person('Çağrı', 'Örskaya', 23, ['Guitar', 'Books'])
-me.setName('Hande Keklik')
+class Employee extends Person {
+  constructor(firstName, lastName, age, likes, position) {
+    super(firstName, lastName, age, likes)
+    this.position = position
+  }
+
+  getBio() {
+    return `${this.firstName} ${this.lastName} is a ${this.position}.`
+  }
+
+  getYearsLeft() {
+    return 65 - this.age
+  }
+}
+
+///////////////// Challenge //////////////////
+// 1) Create Class for students
+// 2) Track student grade. 0 - 100
+// 3) Override bio to print a passing or failing message.
+// 70 and above "Andrew is passing the class", below "Andrew is failing the class"
+// 4) Create "updateGrade" that takes the amount to add or remove from the grade
+
+// Create student
+// Print status (failing or passing)
+// Change grade to change status
+// Print status again
+
+class Student extends Person {
+  constructor(firstName, lastName, age, likes, grade) {
+    super(firstName, lastName, age, likes)
+    this.grade = grade
+  }
+
+  getBio() {
+    if (this.grade >= 70) {
+      return `${this.firstName} ${this.lastName} is passing the class.`
+    } else {
+      return `${this.firstName} ${this.lastName} is failing the class.`
+
+    }
+  }
+
+  updateGrade(change) {
+    this.grade += change
+  }
+}
+
+// Tests
+const me = new Student('Çağrı', 'Örskaya', 23, ['guitars'], 55)
 console.log(me.getBio())
-
-const person2 = new Person('Hakan', 'Örskaya', 35)
-console.log(person2.getBio())
-
+me.updateGrade(20)
+console.log(me.getBio())
