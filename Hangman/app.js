66b96abea5f915d355aa3bccb3556d3f78418dd3
corskaya <cagriorskaya@hotmail.com>
2022-03-04 15:05:13 +0300

    fetched data from countries api

--- Hangman/app.js
+++ Hangman/app.js
@@ -24,11 +24,32 @@ const request = new XMLHttpRequest()
 request.addEventListener('readystatechange', (e) => {
   if (e.target.readyState === 4 && e.target.status === 200) {
     const data = JSON.parse(e.target.responseText)
-    console.log(data)
+    // console.log(data)
   } else if (e.target.readyState === 4) {
-    console.log('En error has taken place')
+    // console.log('En error has taken place')
   }
 })
 
 request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=2')
 request.send()
+
+///////////////////// Challenge //////////////////////
+// 1) Make a new request for all countries
+// 2) Parse the responseText to get back the array of objects
+// 3) Find your contry object by it's country code (alpha2Code property)
+// 4) Print the full country name (name property)
+
+const request2 = new XMLHttpRequest()
+
+request2.open('GET', 'https://restcountries.com/v3.1/all')
+request2.send()
+
+request2.addEventListener('readystatechange', (e) => {
+  if (e.target.readyState === 4 && e.target.status === 200) {
+    const countries = JSON.parse(e.target.responseText)
+    const myCountry = countries.find(country => country.cca2 === 'TR')
+    console.log(myCountry.name.common)
+  } else if (e.target.readyState === 4) {
+    console.log('Error')
+  }
+})
