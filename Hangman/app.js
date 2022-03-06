8321deb9a93e8a5156ecacea12775271b24f1a50
corskaya <cagriorskaya@hotmail.com>
2022-03-06 19:47:53 +0300

    promise challenge part 2 completed

--- Hangman/app.js
+++ Hangman/app.js
@@ -24,19 +24,18 @@ getPuzzle('2').then((puzzle) => {
   console.log(err)
 })
 
-getCountryName('TR').then((country) => {
-  console.log(country)
-}).catch((err) => {
-  console.log(err)
-})
-
 ////////////////// Challenge //////////////////
 // 1) Create getLocation function which takes no argument
 // 2) Setup getLocation to make a request to the url and parse the data
 // 3) Use getLocation to print the city, region, and country information
 
+// Part 2: Call getCountryName method with promise chaining
+
 getLocation().then((location) => {
   console.log(`You are currently at ${location.city}, ${location.region}, ${location.country}`)
+  return getCountryName(location.country)
+}).then((country) => {
+  console.log(country)
 }).catch((err) => {
   console.log(err)
 })
